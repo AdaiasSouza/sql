32bca823adfce20a0b133572fb9093a70ee900fc
AdaiasSouza <adaias_305@hotmail.com>
2023-07-21 15:34:08 -0300

    Arquivos de criacao

---
+++ users.sql
@@ -0,0 +1,32 @@
+# criando novo usuario
+create user usermysql@'%' identified by 'cursomysql';
+grant all privileges on *.* to usermysql@'%' with grant option;
+revoke all on *.* from usermysql;
+
+# privilegio global
+grant all on *.* to usermysql@localhost;
+revoke all on *.* from usermysql;
+
+# privilegio a nivel de banco de dados
+grant all to 'banco_de_dados'.* to usermysql@localhost
+revoke all on 'banco_de_dados'.*;
+
+# privilegio nivel de tabela
+grant all on 'banco_de_dados.nome_tabela';
+revoke all on 'banco_de_dados.nome_tabela';
+
+# privilegio a nivel de coluna
+grant select('nomecoluna1'), insert('nomecoluna1'), update('nomecoluna1')
+		on 'banco_de_dados.nome_tabela' to usermysql@localhost identified by 'senha';
+
+# privilegio a nivel de stored procedures
+grant routine on 'banco_de_dados'.* to usermysql@localhost;
+grant execute on procedure 'banco_de_dados.nomeprocedure' to usermysql@localhost;
+
+# privilegio a nivel de proxy
+grant PROXY on usermysql@localhost to 'usuarioexterno'@'hostexterno';
+
+
+
+
+
