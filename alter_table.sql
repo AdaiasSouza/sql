7130fb1a8e631eefbb4f8e8207cf99fb267122a9
AdaiasSouza <adaias_305@hotmail.com>
2023-07-26 14:59:08 -0300

    Alterações de metadados

---
+++ alter_table.sql
@@ -0,0 +1,4 @@
+-- alter table comclien add column c_cidaclien varchar(50);
+-- alter table comclien add column c_estaclien varchar(50);
+-- alter table comclien modify column c_estaclien int;
+alter table comclien modify column c_estaclien varchar(50);
