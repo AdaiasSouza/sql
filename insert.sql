f06e085e529e4079044ec1d2e33106f69e7a0036
AdaiasSouza <adaias_305@hotmail.com>
2023-07-26 15:01:33 -0300

    Inserção de dados nas tabelas

---
+++ insert.sql
@@ -0,0 +1,11 @@
+insert into comclien
+	values (1,
+			'0001',
+			'AARONSON',
+			'AARONSON FURNITURE LTDA',
+			'2015-02-17',
+			'17.807.928/0001-85',
+			'(21) 8167-6584',
+			'QUEIMADOS',
+			'RJ'
+		);
